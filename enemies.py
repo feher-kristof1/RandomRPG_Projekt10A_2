7d9c6090a58eff265f0da9ec2d84712632ba84fc
feher.kristof1 <feher.kristof1@students.jedlik.eu>
2023-03-28 09:12:07 +0200

    olvasás elkezdése

---
+++ enemies.py
@@ -0,0 +1,95 @@
+from pstats import Stats
+import random
+
+
+class Enemies:
+    @property
+    def Enemyname(self) -> str:
+        randomnumbers: int = 0
+        while randomnumbers != 9:
+            enemyname: str = ''
+            r: int = random.randint(0, 100)
+            randomnumbers += 1
+            if r <= 20:
+                enemyname = 'Bat'
+                return enemyname
+            elif r <= 36 and r > 20:
+                enemyname = 'Zombie'
+                return enemyname
+            elif r <= 52 and r > 36:
+                enemyname = 'Ogre'
+                return enemyname
+            elif r <= 68 and r > 52:
+                enemyname = 'Troll'
+                return enemyname
+            elif r <= 84 and r > 68:
+                enemyname = 'Snake'
+                return enemyname
+            elif r <= 100 and r > 84:
+                enemyname = 'Ghost'
+                return enemyname
+
+    @property
+    def Enemytype(self) -> str:
+        randomnumbers: int = 0
+        while randomnumbers != 9:
+            entype: str = ''
+            r: int = random.randint(0, 100)
+            randomnumbers += 1
+            if r <= 12:
+                entype = '.jpg'
+                return entype
+            elif r <= 26 and r > 12:
+                entype = '.png'
+                return entype
+            elif r <= 40 and r > 26:
+                entype = '.txt'
+                return entype
+            elif r <= 52 and r > 40:
+                entype = '.mp3'
+                return entype
+            elif r <= 64 and r > 52:
+                entype = '.mp4'
+                return entype
+            elif r <= 76 and r > 64:
+                entype = '.rar'
+                return entype
+            elif r <= 88 and r > 76:
+                entype = '.zip'
+                return entype
+            elif r <= 100 and r > 88:
+                entype = '.iso'
+                return entype
+
+    @ property
+    def Enemystats_hp(self) -> int:
+        hp: int = 0
+        return hp
+
+    @ property
+    def Enemystats_atk(self) -> int:
+        atk: int = 0
+        return atk
+
+    @ property
+    def Enemystats_spd(self) -> int:
+        spd: int = 0
+        return spd
+    @property
+    def fileread(self) -> list[str]:
+        enemies: list[str] = []
+        with open('enemies.txt', 'r', encoding='utf-8') as file:
+            for sor in file.read().splitlines()[1:]:
+                enemies.append(sor)
+        return enemies
+    
+    @property
+    def stats_hp(self) -> list[int]:
+        stats: list[int] = []
+        for i in self.fileread:
+            
+        return stats
+
+
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -1,47 +1,14 @@
-import random
+from enemies import Enemies
 
 
 def main() -> None:
-    randomnumbers: int = 0
-    while randomnumbers != 9:
-        r: int = random.randint(0, 100)
-        randomnumbers += 1
-        if r <= 20:
-            enemyname = 'Bat'
-        elif r <= 36 and r > 20:
-            enemyname = 'Zombie'
-        elif r <= 52 and r > 36:
-            enemyname = 'Ogre'
-        elif r <= 68 and r > 52:
-            enemyname = 'Troll'
-        elif r <= 84 and r > 68:
-            enemyname = 'Snake'
-        elif r <= 100 and r > 84:
-            enemyname = 'Ghost'
-              
-    randomnumbers: int = 0
-    while randomnumbers != 9:
-        r: int = random.randint(0, 100)
-        randomnumbers += 1
-        if r <= 12:
-            type = '.jpg'
-        elif r <= 26 and r > 12:
-            type = '.png'
-        elif r <= 40 and r > 26:
-            type = '.txt'
-        elif r <= 52 and r > 40:
-            type = '.mp3'
-        elif r <= 64 and r > 52:
-            type = '.mp4'
-        elif r <= 76 and r > 64:
-            type = '.rar'
-        elif r <= 88 and r > 76:
-            type = '.zip'
-        elif r <= 100 and r > 88:
-            type = '.iso'
+    enemies: Enemies = Enemies()
 
-    print(f'You have been attacked by a(n) {enemyname}{type}')
-
+    print(f'You have been attacked by a(n) {enemies.Enemyname}{enemies.Enemytype}')
+    print(f'It has {enemies.Enemystats_hp} hitpoints.')
+    print(f'It deals {enemies.Enemystats_atk} hitpoints of damage with every hit.')
+    print(f'It has a speed of {enemies.Enemystats_spd}.')
+    print(enemies.fileread[0])
 
 if __name__ == "__main__":
     main()
