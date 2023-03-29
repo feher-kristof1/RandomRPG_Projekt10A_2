26866376083f3a30b167c63619640d6fd933f2fc
feher.kristof1 <feher.kristof1@students.jedlik.eu>
2023-03-29 08:36:11 +0200

    random sorsolás kész

--- enemies.py
+++ enemies.py
@@ -1,124 +1,35 @@
-import random
-
 
 class Enemies:
-    @property
-    def Enemyname(self) -> str:
-        randomnumbers: int = 0
-        while randomnumbers != 1:
-            enemyname: str = ''
-            r: int = random.randint(0, 100)
-            randomnumbers += 1
-            if r <= 20:
-                enemyname = 'Bat'
-                return enemyname
-            elif r <= 36 and r > 20:
-                enemyname = 'Zombie'
-                return enemyname
-            elif r <= 52 and r > 36:
-                enemyname = 'Ogre'
-                return enemyname
-            elif r <= 68 and r > 52:
-                enemyname = 'Troll'
-                return enemyname
-            elif r <= 84 and r > 68:
-                enemyname = 'Snake'
-                return enemyname
-            elif r <= 100 and r > 84:
-                enemyname = 'Ghost'
-                return enemyname
+    Name: str
+    hp: int
+    atk: int
+    spd: int
 
-    @property
-    def Enemytype(self) -> str:
-        randomnumbers: int = 0
-        while randomnumbers != 9:
-            entype: str = ''
-            r: int = random.randint(0, 100)
-            randomnumbers += 1
-            if r <= 12:
-                entype = '.jpg'
-                return entype
-            elif r <= 26 and r > 12:
-                entype = '.png'
-                return entype
-            elif r <= 40 and r > 26:
-                entype = '.txt'
-                return entype
-            elif r <= 52 and r > 40:
-                entype = '.mp3'
-                return entype
-            elif r <= 64 and r > 52:
-                entype = '.mp4'
-                return entype
-            elif r <= 76 and r > 64:
-                entype = '.rar'
-                return entype
-            elif r <= 88 and r > 76:
-                entype = '.zip'
-                return entype
-            elif r <= 100 and r > 88:
-                entype = '.iso'
-                return entype
+    # @property
+    # def fileread(self) -> list[Enemies]:
+    # enemies: list[Enemies] = []
+    # with open('enemies.txt', 'r', encoding='utf-8') as file:
+    #     for sor in file.read().splitlines()[1:]:
+    #         enemies.append(Enemies(sor))
+    # return enemies
 
-    @property
-    def fileread(self) -> list[str]:
-        enemies: list[str] = []
-        with open('enemies.txt', 'r', encoding='utf-8') as file:
-            for sor in file.read().splitlines()[1:]:
-                enemies.append(sor)
-        return enemies
+    def __init__(self, sor: str):
+        stat = sor.split(' ')
+        self.Name = stat[0]
+        self.hp = int(stat[1])
+        self.atk = int(stat[2])
+        self.spd = int(stat[3])
 
-    @property
-    def Stats_hp(self) -> list[int]:
-        HP: list[int] = []
-        while len(HP) != 1:
-            if self.Enemyname == 'Bat' or self.Enemyname == 'Snake':
-                En_hp = 10
-                HP.append(En_hp)
-            elif self.Enemyname == 'Zombie' or self.Enemyname == 'Ghost':
-                En_hp = 20
-                HP.append(En_hp)
-            elif self.Enemyname == 'Ogre' or self.Enemyname == 'Troll':
-                En_hp = 50
-                HP.append(En_hp)
-            elif self.Enemyname == 'Dragon':
-                En_hp = 100
-                HP.append(En_hp)
-        return HP
-    
-    @property
-    def Stats_spd(self) -> list[int]:
-        SPD: list[int] = []
-        while len(SPD) != 1:
-            if self.Enemyname == 'Bat':
-                En_spd = 3
-                SPD.append(En_spd)
-            elif self.Enemyname == 'Zombie' or self.Enemyname == 'Ogre' or self.Enemyname == 'Snake':
-                En_spd = 2
-                SPD.append(En_spd)
-            elif self.Enemyname == 'Ghost' or self.Enemyname == 'Troll':
-                En_spd = 1
-                SPD.append(En_spd)
-            elif self.Enemyname == 'Dragon':
-                En_spd = 100
-                SPD.append(En_spd)
-        return SPD
-    
-    @property
-    def Stats_atk(self) -> list[int]:
-        ATK: list[int] = []
-        while len(ATK) != 1:
-            if self.Enemyname == 'Bat' or self.Enemyname == 'Zombie' or self.Enemyname == 'Ogre':
-                En_atk = 10
-                ATK.append(En_atk)
-            elif self.Enemyname == 'Ghost' or self.Enemyname == 'Snake' or self.Enemyname == 'Troll':
-                En_atk = 20
-                ATK.append(En_atk)
-            elif self.Enemyname == 'Dragon':
-                En_atk = 100
-                ATK.append(En_atk)
-        return ATK
-            
 
-    def __init__(self):
-        pass
+class Enemymods:
+    file_name: str
+    file_hp: float
+    file_atk: float
+    file_spd: int
+
+    def __init__(self, fsor: str):
+        file = fsor.split(' ')
+        self.file_name = file[0]
+        self.file_hp = float(file[1])
+        self.file_atk = float(file[2])
+        self.file_spd = int(file[3])
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from enemies import Enemies
+from enemies import Enemymods
+import random
 
 
 def main() -> None:
-    enemies: Enemies = Enemies()
 
-    print(f'You have been attacked by a(n) {enemies.Enemyname}{enemies.Enemytype}')
+    enemies_list: list[Enemies] = []
+    with open('enemies.txt', 'r', encoding='utf-8') as file:
+        for sor in file.read().splitlines()[1:]:
+            enemies_list.append(Enemies(sor))
+
+    file_list: list[Enemymods] = []
+    with open('files.txt', 'r', encoding='utf-8') as file:
+        for fsor in file.read().splitlines():
+            file_list.append(Enemymods(fsor))
+
+    # print(f'You have been attacked by a(n) {enemies.Enemyname}{enemies.Enemytype}')
     # print(f'It has {enemies.Enemystats_hp} hitpoints.')
     # print(f'It deals {enemies.Enemystats_atk} hitpoints of damage with every hit.')
     # print(f'It has a speed of {enemies.Enemystats_spd}.')
-    print(enemies.Stats_hp)
-    print(enemies.Stats_atk)
-    print(enemies.Stats_spd)
+    randomnumber: int = 0
+    while randomnumber != 1:
+        r = random.randint(0, len(enemies_list) - 1)
+        fr = random.randint(0, len(file_list))
+        randomnumber += 1
+        print(f'{enemies_list[r].Name}{file_list[fr].file_name}')
+        print(enemies_list[r].hp * file_list[fr].file_hp)
+        print(enemies_list[r].atk * file_list[fr].file_atk)
+        print(enemies_list[r].spd * file_list[fr].file_spd)
 
 
 if __name__ == "__main__":
