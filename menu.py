be7effb491a07ada38c4abe89f0257747da8eabc
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2023-03-29 09:04:48 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,8 +1,9 @@
-def menu(hp: int, atk: int, spd: int, enemy_hp: int, enemy_atk: int, enemy_spd: int) -> str:
+def menu(hp: int, atk: int, spd: int, enemy_hp: int, enemy_atk: int, enemy_spd: int, enemy_name: str) -> str:
     hp_ret_string = f'életerőd {hp}'
     atk_ret_string = f'támadási erőd {atk}'
     spd_ret_string = f'sebességed {spd}'
     enemy_hp_ret_string = f'ellenséged életereje {enemy_hp}'
     enemy_atk_ret_string = f'ellenséged támadási ereje {enemy_atk}'
     enemy_spd_ret_string = f'ellenséged sebessége {enemy_spd}'
-    return f'{hp_ret_string}\n{atk_ret_string}\n{spd_ret_string}\n{enemy_hp_ret_string}\n{enemy_atk_ret_string}\n{enemy_spd_ret_string}'
+    enemy_name_ret_string = f'ellenséged {enemy_name}'
+    return f'{hp_ret_string}\n{atk_ret_string}\n{spd_ret_string}\n{enemy_name_ret_string}\n{enemy_hp_ret_string}\n{enemy_atk_ret_string}\n{enemy_spd_ret_string}'
