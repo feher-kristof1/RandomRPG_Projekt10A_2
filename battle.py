08f879ee959ede04d08df0b37ce040ac27f3e042
feher-kristof1 <feher.kristof1@students.jedlik.eu>
2023-05-29 13:43:23 +0200

    Dekorálás rich-el, dizájnolás

--- battle.py
+++ battle.py
@@ -1,122 +1,135 @@
+from rich.console import Console
 from menu import menu
+
+console = Console()
 
 
 def player_input():
     while True:
-        inp: str = (input("kérem a játékos lépését [1 -> támadás, 2 -> Futás, 3 -> magic, 4 -> item]: "))
+        console.print('Lehetőségeid:', style='bright_white')
+        console.print('\t1 -> Támadás;\n\t2 -> Futás;\n\t3 -> Mágia használata;\n\t4 -> Item használata;', style='bright_white')
+        inp: str = (input("Mit akarsz tenni? "))
         if inp not in ["1", "2", "3", "4"]:
-            print("Hibás input")
+            console.print("Hibás input, újra!", style='bright_red')
         else:
             return int(inp)
 
 
 def battle(hp: int, spd: int, atk: int, mana: int, item: int, enemy_hp: int, enemy_spd: int, enemy_atk: int, enemy_name: str):
-    dev = input("enable dev power?[Y/N]?")
+    dev = input("Press enter to start this round!")
     if dev == "Y":
-        print("YOU WIN")
+        console.print("YOU WIN", style='gold1')
         return False, (hp if hp > 0 else 0)
     enemy_max_atk: int = enemy_atk
     while hp > 0 and enemy_hp > 0:
-        print(menu(hp, atk, spd, mana, item, enemy_hp, enemy_atk, enemy_spd, enemy_name))
+        console.print(f'{menu(hp, atk, spd, mana, item, enemy_hp, enemy_atk, enemy_spd, enemy_name)}', style='grey62')
         player_input_value: int = int(player_input())
         if spd > enemy_spd:
             if player_input_value == 1:
                 enemy_hp -= atk
-                print(f"Az ellenség életereje {atk} értékkel csökkent, {enemy_hp} élete maradt")
+                console.print(f"Az ellenség életereje {atk} értékkel csökkent, {enemy_hp} élete maradt", style='red on black')
             elif player_input_value == 2:
-                print("Sikeres menekülés")
+                console.print("Sikeres menekülés", style='yellow on black')
                 return False, (hp if hp > 0 else 0)
             elif player_input_value == 3:
-                magic_select: int = int(input("válassz varázslatot[1 -> healing(+30HP)(5 mana), 2 ->enemy atk down(-7atk)(3 mana), 3 -> your atk up(+10atk)(4mana)]: "))
+                console.print('Varázslataid:', style='turquoise4')
+                console.print('\t1 -> Healing:(+30 HP),(5 mana);\n\t2 -> Enemy atk down:(-7 atk),(3 mana);\n\t3 -> Your atk up:(+10 atk),(4 mana);', style='turquoise4')
+                magic_select: int = int(input("Válassz varázslatot:"))
                 if magic_select == 1 and mana >= 5:
-                    print("karakter élete +30")
+                    console.print("Karakter élete +30", style='green')
                     hp += 30
                     mana -= 5
                 elif magic_select == 2 and mana >= 3:
-                    print("ellenség támadása -7")
+                    console.print("Ellenség támadása -7", style='dark_olive_green2')
                     if enemy_atk - 7 < 0:
                         enemy_atk = 0
                     else:
                         enemy_atk -= 7
                     mana -= 3
                 elif magic_select == 3 and mana >= 4:
-                    print("támadásod +10")
+                    console.print("Támadásod +10", style='dark_olive_green2')
                     atk += 10
                     mana -= 4
                 else:
                     print("NO MANA")
 
             elif player_input_value == 4:
-                item_select: int = int(input("melyik itemet használod[1-> vulnerary(+50HP), 2 -> killer(enemy HP halved)]: "))
+                console.print('Itemjeid:', style='orange3')
+                console.print('\t1 -> Vulnerary: +50HP;\n\t2 -> Killer: Enemy HP halved;', style='orange3')
+                item_select: int = int(input("Válassz itemet: "))
                 if item_select == 1 and item >= 1:
-                    print("vulnerary használva(+50HP)")
+                    console.print("Vulnerary használva: +50HP", style='orange4')
                     hp += 50
                     item -= 1
                 elif item_select == 2 and item >= 1:
-                    print("killer használva(enemy HP/2)")
+                    console.print("Killer használva: Enemy HP halved", style='orange4')
                     enemy_hp = int(enemy_hp / 2)
                     item -= 1
                 else:
                     print("NO ITEMS")
             hp -= enemy_atk
             if hp < 0:
-                print(f"A játékos életereje {enemy_atk} értékkel csökkent, meghalt")
+                console.print(f"A játékos életereje {enemy_atk} értékkel csökkent, meghalt", style='black on red')
             else:
-                print(f"A játékos életereje {enemy_atk} értékkel csökkent, {hp} élete maradt")
+                console.print(f"A játékos életereje {enemy_atk} értékkel csökkent, {hp} élete maradt", style='green on black')
             mana += 1
         else:
             hp -= enemy_atk
             if hp < 0:
-                print(f"A játékos életereje {enemy_atk} értékkel csökkent, meghalt")
+                console.print(f"A játékos életereje {enemy_atk} értékkel csökkent, meghalt", style='black on red')
             else:
-                print(f"A játékos életereje {enemy_atk} értékkel csökkent, {hp} élete maradt")
+                console.print(f"A játékos életereje {enemy_atk} értékkel csökkent, {hp} élete maradt", style='green on black')
             if player_input_value == 1:
                 enemy_hp -= atk
                 if hp < 0:
-                    print(f"Az ellenség életereje {atk} értékkel csökkent, meghalt")
+                    console.print(f"Az ellenség életereje {atk} értékkel csökkent, meghalt", style='black on yellow')
                 else:
-                    print(f"Az ellenség életereje {atk} értékkel csökkent, {enemy_hp} élete maradt")
+                    console.print(f"Az ellenség életereje {atk} értékkel csökkent, {enemy_hp} élete maradt", style='red on black')
             elif player_input_value == 2:
-                print("Sikertelen menekülés")
+                console.print("Sikertelen menekülés", style='orange3')
             elif player_input_value == 3:
-                magic_select: int = int(input("válassz varázslatot[1 -> healing(+30HP)(5 mana), 2 ->enemy atk down(-7atk)(3 mana), 3 -> your atk up(+10atk)(4mana)]: "))
+                console.print('Varázslataid:', style='turquoise4')
+                console.print('\t1 -> Healing:(+30HP),(5 mana);\n\t2 -> Enemy atk down:(-7atk),(3 mana);\n\t3 -> Your atk up:(+10atk),(4 mana);', style='turquoise4')
+                magic_select: int = int(input("Válassz varázslatot:"))
                 if magic_select == 1 and mana >= 5:
-                    print("karakter élete +30")
+                    console.print("Karakter élete +30", style='green')
                     hp += 30
                     mana -= 5
                 elif magic_select == 2 and mana >= 3:
-                    print("ellenség támadása -7")
+                    console.print("Ellenség támadása -7", style='dark_olive_green2')
                     if enemy_atk - 7 < 0:
                         enemy_atk = 0
                     else:
                         enemy_atk -= 7
                     mana -= 3
                 elif magic_select == 3 and mana >= 4:
-                    print("támadásod +10")
+                    console.print("Támadásod +10", style='dark_olive_green2')
                     atk += 10
                     mana -= 4
                 else:
                     print("NO MANA")
             elif player_input_value == 4:
-                item_select: int = int(input("melyik itemet használod[1-> vulnerary(+50HP), 2 -> killer(enemy HP halved)]: "))
+                console.print('Itemjeid:', style='orange3')
+                console.print('1-> Vulnerary: +50HP;\n\t 2 -> Killer: Enemy HP halved;', style='ornage3')
+                item_select: int = int(input("Válassz itemet: "))
                 if item_select == 1 and item >= 1:
-                    print("vulnerary használva(+50HP)")
+                    console.print("Vulnerary használva: +50HP", style='orange4')
                     hp += 50
                     item -= 1
                 elif item_select == 2 and item >= 1:
-                    print("killer használva(enemy HP/2)")
+                    console.print("Killer használva: Enemy HP halved", style='orange4')
                     enemy_hp = int(enemy_hp / 2)
                     item -= 1
                 else:
-                    print("NO ITEMS")
+                    console.print("NO ITEMS", style='dark_red')
             mana += 1
         if enemy_atk + 1 > enemy_max_atk:
             pass
         else:
             enemy_atk += 1
     if enemy_hp > 0:
-        print("YOU LOSE")
+        console.print("YOU LOSE", style='grey85 on grey3')
         return True, (hp if hp > 0 else 0)
     elif hp > 0:
-        print("YOU WIN")
+        console.print("YOU WIN", style='gold1')
         return False, (hp if hp > 0 else 0)
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import pygame
+from battle import battle
+from enemies import Enemymods
+from enemies import Enemies
+from char_class import Playerstats
+import time
+import os
+import random
+from pygame.locals import *
+
+
 def reader(filename, loader_class, skip_num):
     target_list = []
     with open(filename, "r", encoding='utf-8') as file:
@@ -5,35 +16,27 @@ def reader(filename, loader_class, skip_num):
             target_list.append(loader_class(sor))
     return target_list
 
-import pygame
-from pygame.locals import *
-import random
-import os
-import time
-from char_class import Playerstats
-from enemies import Enemies
-from enemies import Enemymods
-from battle import battle
 
 pygame.init()
+
 
 def main() -> None:
     pygame.mixer.music.load("battle.ogg")
     pygame.mixer.music.play(-1, 0.0)
     enemies_list: list[Enemies] = []
-    #with open('enemies.txt', 'r', encoding='utf-8') as file:
+    # with open('enemies.txt', 'r', encoding='utf-8') as file:
     #    for sor in file.read().splitlines()[1:]:
     #        enemies_list.append(Enemies(sor))
     enemies_list = reader('enemies.txt', Enemies, 1)
 
     # file_list: list[Enemymods] = []
-    #with open('files.txt', 'r', encoding='utf-8') as file:
+    # with open('files.txt', 'r', encoding='utf-8') as file:
     #    for fsor in file.read().splitlines():
     #        file_list.append(Enemymods(fsor))
     file_list = reader('files.txt', Enemymods, 0)
 
     playerstat_list: list[Playerstats] = []
-    #with open('player.txt', 'r', encoding='utf-8') as file:
+    # with open('player.txt', 'r', encoding='utf-8') as file:
     #    for psor in file.read().splitlines():
     #        playerstat_list.append(Playerstats(psor))
     playerstat_list = reader('player.txt', Playerstats, 0)
@@ -66,7 +69,6 @@ def main() -> None:
     rv = r
     frv = fr
     rs = random.randint(0, len(playerstat_list) - 1)
-    
 
     mana: int = 10
     hp: int = playerstat_list[rs].player_hp + (hppoints * 10)
@@ -83,11 +85,11 @@ def main() -> None:
         enemy_atk: int = int(enemies_list[rv].atk * file_list[frv].file_atk)
         enemy_spd: int = int(enemies_list[rv].spd * file_list[frv].file_spd)
         rs = random.randint(0, len(playerstat_list) - 1)
-        
+
         stage_calc += 1
 
         print(f'{stage_calc}. pálya')
-        
+
         if stage_calc == 5:
             pygame.mixer.music.load("boss1.mp3")
             pygame.mixer.music.play(-1, 0.0)
@@ -165,7 +167,7 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
+inpp: str = ''
 while inpp != "N":
     pygame.mixer.music.load("menu.mp3")
     pygame.mixer.music.play(-1, 0.0)
